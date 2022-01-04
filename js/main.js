b356ff3f7f1e9c881f2d596527fcae9fcf15b005
Alex Begg <alex.begg@gmail.com>
2022-01-03 22:24:42 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $('#video').hover(function toggleControls() {
         this.setAttribute("controls", "controls")
     }
 });
-$('.video').click(function () {
-  if($(this).children("#video").get(0).paused){
-      $(this).children("#video").get(0).play();
-      $(this).children(".playpause").fadeOut();
+$('#video').parent().click(function () {
+    if($(this).children("#video").get(0).paused){
+        $(this).children("#video").get(0).play();
+        $(this).children(".playpause").fadeOut();
     } else {
         $(this).children("#video").get(0).pause();
         $(this).children(".playpause").fadeIn();
